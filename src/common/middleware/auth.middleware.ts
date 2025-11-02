efbe9d560b07cc463cd57820c91d395d08b8f605
LTN130925 <ltansang374@gmail.com>
2025-11-02 03:10:19 +0000

    sửa lại phần đăng nhập giữa máy chủ và người dùng

--- src/common/middleware/auth.middleware.ts
+++ src/common/middleware/auth.middleware.ts
@@ -1,6 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
+import prefixNameConfig from "../config/prefixName.config";
 
 export const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
-    if (req.isAuthenticated()) res.locals.user = req.user;
+    if (req.isAuthenticated() && req.user['roleId']) {
+        req.flash('error', 'Vui lòng đăng nhập!')
+        return res.redirect('/auth/login');
+    }
+    res.locals.user = req.user;
     next();
 };
--- src/common/middleware/authServer.middleware.ts
+++ src/common/middleware/authServer.middleware.ts
@@ -5,7 +5,11 @@ import prefixNameConfig from '../config/prefixName.config';
 export const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
     if (!req.isAuthenticated()) {
         req.flash('error', 'Vui lòng đăng nhập');
-        res.redirect(prefixNameConfig.PATH_ADMIN + '/auth/login');
+        return res.redirect(prefixNameConfig.PATH_ADMIN + '/auth/login');
+    }
+    if (!req.user['roleId']) {
+        req.flash('error', 'Vui lòng đăng nhập');
+        return res.redirect(prefixNameConfig.PATH_ADMIN + '/auth/login');
     }
     res.locals.manager = req.user;
     next();
