a9566cbe5835688c076a6b47123b1888cdf22d11
LTN130925 <ltansang374@gmail.com>
2025-11-16 07:03:12 +0000

    cấu hình trang client phần thông báo và nút đăng kí

---
+++ public/client/js/subscribe.js
@@ -0,0 +1,35 @@
+const btnRegisterSinger = document.querySelectorAll('.button-register-singer');
+
+if (btnRegisterSinger.length > 0) {
+    btnRegisterSinger.forEach(btn => {
+        btn.addEventListener('click', async () => {
+
+            const span = btn.querySelector('span');
+            const singerId = span.getAttribute('data-id');
+
+            const isActive = btn.classList.contains('active');
+            const type = isActive ? 'unsubscribe' : 'subscribe';
+
+            const link = `/singer/${type}/${singerId}`;
+
+            try {
+                const res = await fetch(link, { method: 'PATCH' });
+                const data = await res.json();
+
+                if (data.message !== 'success') return;
+
+                // toggle class
+                btn.classList.toggle('active');
+
+                // icon animation
+                const icon = btn.querySelector('i');
+                icon.style.animation = 'none';
+                icon.offsetHeight; // reset
+                icon.style.animation = '';
+
+            } catch (err) {
+                console.error('Lỗi đăng ký ca sĩ:', err);
+            }
+        });
+    });
+}
--- src/module/client/service/auth.service.ts
+++ src/module/client/service/auth.service.ts
@@ -5,6 +5,7 @@ import {SongLikeModel} from '../../../common/model/songLike.model';
 import {SongViewModel} from '../../../common/model/songView.model';
 import {SongFavouriteModel} from '../../../common/model/songFavourite.model';
 import {SubscribersModel} from '../../../common/model/subscribers.model';
+import {MassagesModel} from '../../../common/model/message.model';
 
 export class authService {
     async register(fullName: string, email: string, password: string) {
@@ -12,11 +13,12 @@ export class authService {
         if (existingUser) return null;
 
         const hashedPassword = await bcrypt.hash(password, 10);
-        const [newSongView, newSongFavourite, newSongLike, newSubscribers] = await Promise.all([
+        const [newSongView, newSongFavourite, newSongLike, newSubscribers, newMassage] = await Promise.all([
             new SongViewModel().save(),
             new SongFavouriteModel().save(),
             new SongLikeModel().save(),
             new SubscribersModel().save(),
+            new MassagesModel().save(),
         ]);
 
         const newUser = new UserModel({
@@ -27,6 +29,7 @@ export class authService {
             listFavoritesSong: newSongFavourite._id,
             listViewsSong: newSongView._id,
             subscribers: newSubscribers._id,
+            messageId: newMassage._id,
         });
         await newUser.save();
 
