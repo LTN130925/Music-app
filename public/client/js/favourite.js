bd12e2bcae7929a4a744fd8a3bae4ce6bb4b04c4
LTN130925 <ltansang374@gmail.com>
2025-11-17 06:33:51 +0000

    sửa lỗi phần yêu thích bài hát

--- public/client/js/favourite.js
+++ public/client/js/favourite.js
@@ -1,40 +1,27 @@
-const btnRegisterSinger = document.querySelectorAll('.button-register-singer');
+const btnFavourite = document.querySelectorAll('.button-favourite');
 
-if (btnRegisterSinger.length > 0) {
-    btnRegisterSinger.forEach(btn => {
-        btn.addEventListener('click', async () => {
-
-            const span = btn.querySelector('span[data-id]');
-            const singerId = span.getAttribute('data-id');
-
-            const isActive = btn.classList.contains('active');
-            const type = isActive ? 'unsubscribe' : 'subscribe';
-
-            const link = `/singer/${type}/${singerId}`;
+if (btnFavourite.length > 0) {
+    btnFavourite.forEach(btnFavourite => {
+        btnFavourite.addEventListener('click', async () => {
+            const span = btnFavourite.querySelector('span');
+            const getDataID = span.getAttribute('data-id');
+            const isActive = btnFavourite.classList.contains('active');
+            const typeFav = isActive ? 'disfav' : 'fav';
+            const link = `/song/favourite/${typeFav}/${getDataID}`;
 
             try {
-                const res = await fetch(link, { method: 'PATCH' });
-                const result = await res.json();
-
-                if (result.message !== 'success') return;
-
-                // toggle class
-                btn.classList.toggle('active');
-
-                // icon animation
-                const icon = btn.querySelector('i');
-                icon.style.animation = 'none';
-                icon.offsetHeight; // reset
-                icon.style.animation = '';
-
-                // ========== CẬP NHẬT SỐ ĐĂNG KÝ ==========
-                const metaSpan = btn.closest('.artist-info').querySelector('.meta span');
-                if (metaSpan) {
-                    metaSpan.innerHTML = `<i class="fas fa-id-card"></i> ${result.data} đăng ký`;
-                }
-
+                fetch(link, {method: 'PATCH'})
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.message !== 'success') return;
+                        btnFavourite.classList.toggle('active');
+                        const icon = btnFavourite.querySelector('i');
+                        icon.style.animation = 'none';
+                        icon.offsetHeight; // force reflow
+                        icon.style.animation = ''; // animation sẽ chạy lại
+                    });
             } catch (err) {
-                console.error('Lỗi đăng ký ca sĩ:', err);
+                console.error('Lỗi khi yêu thích bài hát:', err);
             }
         });
     });
