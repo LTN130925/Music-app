3a61d8af80c7c402bc1d22abc9bffc55aaebf596
LTN130925 <ltansang374@gmail.com>
2025-12-03 09:47:50 +0000

    tạo ra hàm kiểm tra quyền và cấu hình quyền truy cập server user

--- src/module/admin/route/user.route.ts
+++ src/module/admin/route/user.route.ts
@@ -11,16 +11,44 @@ import { idValidate } from '../../../common/validate/id.validate';
 import { statusValidate } from '../../../common/validate/status.validate';
 import { changeMultiValidate } from '../../../common/validate/changeMulti.validate';
 
+// permission middleware
+import { checkPermission } from "../../../common/middleware/checkPermisson.middleware";
+
 // ------------------- ROUTES -------------------
 
-router.get('/', controllerInstance.index);
+router.get(
+    '/',
+    checkPermission('user_view'),
+    controllerInstance.index
+);
 
-router.get('/detail/:id', idValidate, controllerInstance.detail);
+router.get(
+    '/detail/:id',
+    checkPermission('user_view'),
+    idValidate,
+    controllerInstance.detail
+);
 
-router.patch('/change-status/:id', idValidate, statusValidate, controllerInstance.changeStatus);
+router.patch(
+    '/change-status/:id',
+    checkPermission('user_edit'),
+    idValidate,
+    statusValidate,
+    controllerInstance.changeStatus
+);
 
-router.delete('/delete/:id', idValidate, controllerInstance.delete);
+router.delete(
+    '/delete/:id',
+    checkPermission('user_delete'),
+    idValidate,
+    controllerInstance.delete
+);
 
-router.patch('/change-multi', changeMultiValidate, controllerInstance.changeMulti);
+router.patch(
+    '/change-multi',
+    checkPermission('user_edit'),
+    changeMultiValidate,
+    controllerInstance.changeMulti
+);
 
 export default router;
