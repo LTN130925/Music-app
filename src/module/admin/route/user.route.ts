5685074d8f69e6cac6c19a9b93fd4f70381d9d23
LTN130925 <ltansang374@gmail.com>
2025-12-03 08:43:49 +0000

    cấu hình kt dữ liệu đầu vào trang user

--- src/module/admin/route/user.route.ts
+++ src/module/admin/route/user.route.ts
@@ -1,23 +1,26 @@
-import {Router} from 'express';
-import multer from 'multer';
-
-const upload = multer();
+import { Router } from 'express';
 
 const router = Router();
 
-import {uploadSingle} from '../../../common/middleware/upload.middleware';
-
-import {controller} from '../controller/user.controller'
+// controller
+import { controller } from '../controller/user.controller';
 const controllerInstance = new controller();
+
+// validate
+import { idValidate } from '../../../common/validate/id.validate';
+import { statusValidate } from '../../../common/validate/status.validate';
+import { changeMultiValidate } from '../../../common/validate/changeMulti.validate';
+
+// ------------------- ROUTES -------------------
 
 router.get('/', controllerInstance.index);
 
-router.get('/detail/:id', controllerInstance.detail);
+router.get('/detail/:id', idValidate, controllerInstance.detail);
 
-router.patch('/change-status/:id', controllerInstance.changeStatus);
+router.patch('/change-status/:id', idValidate, statusValidate, controllerInstance.changeStatus);
 
-router.delete('/delete/:id', controllerInstance.delete);
+router.delete('/delete/:id', idValidate, controllerInstance.delete);
 
-router.patch('/change-multi', controllerInstance.changeMulti);
+router.patch('/change-multi', changeMultiValidate, controllerInstance.changeMulti);
 
 export default router;
